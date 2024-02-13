8bf8e007c9e68c9823a280888b87b2fc7f86313c
Mychel Garzon <mtjomy7@gmail.com>
2024-02-13 14:52:10 +0200

    Add MongoDB and Mongoose dependencies, update app.ts to connect to MongoDB, and modify todo.ts and todo.ts to include commented out routes and controllers

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -1,13 +1,24 @@
 import { RequestHandler } from 'express';
-import { Todo } from '../models/todo';
+import Todo, { TodoModel } from '../models/todo';
 
-const TODOS: Todo[] = [];
-
-export const createTodo: RequestHandler = (req, res, next) => {
+export const createTodo: RequestHandler = async (req, res, next) => {
   //const text = req.body.text;
+  try {
+    const data: TodoModel = req.body;
+    console.log('Data: ', data);
+    let todos = await Todo.create(data);
+    return res
+      .status(200)
+      .json({ message: 'Todo created successfully', todos });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+/*
   const text = (req.body as { text: string }).text; // Type casting
   const newTodo = new Todo(Math.random().toString(), text);
-  TODOS.push(newTodo);
+  //TODOS.push(newTodo);
   res.status(201).json({ message: 'Created the todo.', createTodo: newTodo });
 };
 
@@ -25,3 +36,14 @@ export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
   TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
   res.json({ message: 'Updated!', updatedTodo: TODOS[todoIndex] });
 };
+
+export const deleteTodo: RequestHandler = (req, res, next) => {
+  const todoId = req.params.id;
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) {
+    throw new Error('Could not find todo!');
+  }
+  TODOS.splice(todoIndex, 1);
+  res.json({ message: 'Todo deleted!' });
+};
+*/
