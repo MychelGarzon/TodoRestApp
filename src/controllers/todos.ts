01dac53bda6b5a1747774f320baeae6b616cf46d
Mychel Garzon <mtjomy7@gmail.com>
2024-02-13 11:59:09 +0200

    Add types for Express and body-parser

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -8,4 +8,5 @@ export const createTodo: RequestHandler = (req, res, next) => {
   const text = (req.body as { text: string }).text;
   const newTodo = new Todo(Math.random().toString(), text);
   TODOS.push(newTodo);
+  res.status(201).json({ message: 'Created the todo.', createTodo: newTodo });
 };
